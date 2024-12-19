ec108418f6b03528c2590b11f4b0d8eeceaab150
FigoZhu <figozhu@gmail.com>
2024-12-19 16:32:07 +0800

    filter not support cipher type for Stash

--- src/utils/filterForStash.ts
+++ src/utils/filterForStash.ts
@@ -3,9 +3,14 @@ import YAML from 'yaml';
 /**
  * stash不支持包含以下内容的节点
  * flow: xtls-rprx-vision
+ * cipher: 2022-blake3-aes-256-gcm
  */
 export function filterProxyForStash(proxy: any) : boolean {
   if (proxy.flow && proxy.flow === 'xtls-rprx-vision') {
+    return false
+  }
+
+  if (proxy.cipher && proxy.cipher === '2022-blake3-aes-256-gcm') {
     return false
   }
 
